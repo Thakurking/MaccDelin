21ce6f052a6be8f350bb51f693c371642cadb814
Ritesh Thakur <riteshkumar3310@gmail.com>
2021-05-11 23:47:56 +0530

    cleaned some code

--- admin-service/routes/auth-route/admin.auth.js
+++ admin-service/routes/auth-route/admin.auth.js
@@ -5,6 +5,5 @@ const adminRouter = express.Router();
 const adminRegisterController = require("../../controller/admin.auth.Controller/admin.register.Controller");
 
 adminRouter.post("/create-admin", adminRegisterController.AdminRegister);
-adminRouter.get("/get-admins", adminRegisterController.GetAdmin);
 
 module.exports = adminRouter;
