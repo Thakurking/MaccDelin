20fc7e6ea1ec657036ce91e9fceea797e021366e
Ritesh Thakur <riteshkumar3310@gmail.com>
2021-06-09 23:30:44 +0530

    made blog model

--- blog-service/models/blog.model.js
+++ blog-service/models/blog.model.js
@@ -2,16 +2,63 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const blogSchema = new Schema({
-  author: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    refPath: "onModel",
+const blogSchema = new Schema(
+  {
+    author: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      refPath: "onModel",
+    },
+    onModel: {
+      type: String,
+      required: true,
+      enum: ["editor", "admin", "author"],
+    },
+    Body: {
+      type: String,
+      required: true,
+    },
+    Comments: [
+      {
+        text: {
+          type: String,
+        },
+        postedBy: {
+          type: Schema.Types.ObjectId,
+          refPath: "onModel",
+        },
+      },
+    ],
+    Photo: {
+      type: String,
+      default: null,
+    },
+    Mp3: {
+      type: String,
+      default: null,
+    },
+    PodcastLink: {
+      type: String,
+      default: null,
+    },
+    Map: {
+      type: String,
+      default: null,
+    },
+    MenuTag: {
+      type: String,
+      required: true,
+    },
+    Status: {
+      type: String,
+      default: "F",
+    },
+    Likes: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "onModel",
+      },
+    ],
   },
-  onModel: {
-    type: String,
-    required: true,
-    enum: ["editor", "admin", "author"],
-  },
-  
-});
+  { timestamps: true }
+);
